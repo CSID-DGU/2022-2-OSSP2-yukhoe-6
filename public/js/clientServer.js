26760fd6ad5dddc8c51d54a06306ed71e528c954
Jang Jidam <jidam03@dongguk.edu>
2022-11-19 06:12:24 +0900

    error during webrtc 7 not fixed yet

--- public/js/clientServer.js
+++ public/js/clientServer.js
@@ -36,7 +36,7 @@ let cameraOff =false;
 async function getMedia(deviceId){
     const initalConstraints = { //deviceId가 없을 때 
         audio : true,
-        vide : {facingMode : "user"}, //카메라가 전, 후면에 둘다 있을 경우 전면 카메라를 선택, 후면은 "environment"
+        video : {facingMode : "user"}, //카메라가 전, 후면에 둘다 있을 경우 전면 카메라를 선택, 후면은 "environment"
     }
     const cameraConstraints = { // cameraConstraints는 deviceId가 있을 때 실행 
         audio : true,
@@ -47,10 +47,13 @@ async function getMedia(deviceId){
         //getUserMedai에 constraint 객체를 전달함. constraint객체는 video와 audio로 구성 
         //요청할 미디어 유형에 대해 설명함 
         //카메라 정보를 얻어왔으니 video를 설정해주기 위함 
-        myStream = await navigator.mediaDevices.getUserMedia(
+  
+        myStream = await navigator.mediaDevices.getUserMedia( 
             //deviceId가 있는지에 따라 
             deviceId ? cameraConstraints : initalConstraints
         )
+       
+        
         myFace.srcObject = myStream; //가져온 video 정보로 뷰페이저의 video 요소 지정 
         if (!deviceId){ // 맨 처음 접속할 때만 (맨 처음 getMedia를 호출할 때)
             await getCameras();
@@ -92,6 +95,8 @@ async function handleCameraChange(){
 //음소거버튼클릭 
 function handleMuteClick(){
     //스트림의 오디오의 enabled를 true->false , false->true로 바꿔줌 
+
+    //예외발생
     myStream.getAudioTracks().forEach(track=>track.enabled = !track.enabled);
 
     //버튼 텍스트랑 오디오 상태 변수 바꾸기 
@@ -121,7 +126,91 @@ function handleCameraClick(){
     }
 
 }
-getMedia();
+
+//방 입력폼 welcome이랑 방을 표시할 call을 뷰페이져에서 가져옴 
+const welcome = document.getElementById(`welcome`);
+const call = document.getElementById(`call`);
+//방 입력 폼 가져옴 
+const welcomeForm = welcome.querySelector(`form`);
+let roomName; //방 이름 
+let myPeerConnection; //누군가 getMedia 함수를 불렀을 때와 똑같이 stream을 공유하기 위한 함수 
+
+
+//방 이름을 입력했으면 입력 폼을 숨기고 방을 보여준다 
+function startMedia(){
+    welcome.hidden = true;
+    call.hidden = false;
+    getMedia();
+    
+    //RTC 연결해주는 함수 
+    makeConnection();
+
+}
+
+//입력한 방 이름을 서버로 보내는 작업 
+function handleWelcomeSubmit(event){
+    
+    //default 동작을 수행하는 것을 방지함 
+    //예를들어 form의 버튼을 submit하면 페이지가 넘어가거나 새로고침되는데 그것을 막음 
+    event.preventDefault();
+
+    //입력 폼의 input 요소 가져옴 
+    const input = welcomeForm.querySelector(`input`);
+
+    //emit으로 이벤트 등록, join_room 이벤트, input.value랑 startMedia 함수를 같이보냄 
+    //서버로 사용자가 입력한 방 이름을 보낸다, startMedia 함수를 같이 보내서 폼을 숨기고 방을 보여줌 
+    socket.emit(`join_room`,input.value,startMedia);
+    //방 이름을 받아옴 
+    roomName = input.value;
+
+    input.value=``;
+
+}
+
+//방 이름 입력 폼에 이벤트리스너 등록 
+welcomeForm.addEventListener('submit',handleWelcomeSubmit);
+
+
+
+
+//방에 접속하고 나서 보여야 하니까 주석처리해줌 
+//getMedia();
+
+
+//일단 방은 숨김 
+call.hidden = true;
+
+
+//누군가 접속하면 welcome 이벤트를 방 접속자에게 전달하도록 만든다 
+socket.on(`welcome`,()=>{
+    console.log(`someone joined`);
+})
+
+//RTC code 
+function makeConnection(){
+    //peerConnection을 각각의 브라우저에 생성함 
+    myPeerConnection = new RTCPeerConnection();
+
+    //myPeerConnection에 영상, 음성 트랙을 추가함 : P2P 연결 
+    console.log(myStream);
+    myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream)); 
+
+}
+
+socket.on(`welcome`,async()=>{
+    const offer = await myPeerConnection.createOffer(); //다른 사용자를 초대하기 위한 초대장 (내가 누구인지를 알려주는 내용 포함)
+    myPeerConnection.setLocalDescription(offer); //myPeerConnection에 내 초대장의 위치 정보를 연결해주는 과정 
+    console.log(`sent the offer`);
+    //offer 이벤트 등록 
+    socket.emit(`offer`,offer,roomName);
+})
+
+//offer 이벤트 처리 
+socket.on(`offer`,offer=>{
+    console.log(offer);
+})
+
+
 
 //버튼에 이벤트 등록 
 muteBtn.addEventListener("click",handleMuteClick);
