485c5bcb221edd8596ae5f382ac903019e00f044
green-yoon87 <wltjd3459@dongguk.edu>
2022-11-18 11:11:41 +0900

    실시간 채팅 기능 추가

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
@@ -7,6 +9,9 @@ var session = require('express-session');
 var passport = require('./config/passport');
 var util = require('./util');
 var app = express();
+const http = require('http');
+const server = http.createServer(app);
+
 
 //mongodb와 node.js 연동
 mongoose
@@ -54,8 +59,30 @@ app.use('/comments', util.getPostQueryString, require('./routes/comments'));
 app.use('/files', require('./routes/files'));
 app.use(`/competitions`,require(`./routes/competitionRouter`));
 
+
 // Port setting
 var port = 3000;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
+
+var io = require('socket.io')(server);
+app.get('/chats', (req, res) => {
+  res.render('chat');    // index.ejs을 사용자에게 전달
+})
+
+io.on('connection', (socket) => {   //연결이 들어오면 실행되는 이벤트
+  // socket 변수에는 실행 시점에 연결한 상대와 연결된 소켓의 객체가 들어있다.
+  
+  //socket.emit으로 현재 연결한 상대에게 신호를 보낼 수 있다.
+  socket.emit('usercount', io.engine.clientsCount);
+
+  // on 함수로 이벤트를 정의해 신호를 수신할 수 있다.
+  socket.on('message', (msg) => {
+      //msg에는 클라이언트에서 전송한 매개변수가 들어온다. 이러한 매개변수의 수에는 제한이 없다.
+      console.log('Message received: ' + msg);
+
+      // io.emit으로 연결된 모든 소켓들에 신호를 보낼 수 있다.
+      io.emit('message', msg);
+  });
+});
+server.listen(port, function() {
+  console.log('Listening on http://localhost:3000/');
 });
