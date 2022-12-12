a8320d9c2da255ea171e4a206841cbad8ee870b3
unknown <2020111858@dgu.ac.kr>
2022-12-12 19:13:37 +0900

    스터디룸 인원수 4(model.studyromm 및 if,else 내부 인원수 수정), 로그인 배치 수정

--- models/StudyRoom.js
+++ models/StudyRoom.js
@@ -6,7 +6,7 @@ var studyRoomSchema = mongoose.Schema({
   //user와 스터디룸 연결 
   leader : {type:mongoose.Schema.Types.ObjectId, ref:'user',required:true},
   date : {type:Date, default:Date.now},
-  maximum : {type:Number,required:[true,'인원수를 입력해주세요!'], default:2, min:1, max:[5,'최대인원수는 5명입니다!']},
+  maximum : {type:Number,required:[true,'인원수를 입력해주세요!'], default:2, min:1, max:[4,'최대인원수는 4명입니다!']},
   content : {type:String, required:[true,'스터디 설명을 작성해주세요!']},
 });
 
